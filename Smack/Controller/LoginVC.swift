b9f4d9112e6c80a912c3341eb8d4c739b6daea42
Abdullah Md. Yusuf <65698102+AM-Yusuf@users.noreply.github.com>
2020-09-12 15:58:24 +0900

    Done Login page UI elements

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Kalani Poomaihealani on 2020/09/12.
+//  Copyright © 2020 AM-Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Kalani Poomaihealani on 2020/09/12.
+//  Copyright © 2020 AM-Yusuf. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
