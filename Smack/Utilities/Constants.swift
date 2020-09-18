7d4afd9a72d7413e247d357b644c88ca8c4a7e9e
Abdullah Md. Yusuf <65698102+AM-Yusuf@users.noreply.github.com>
2020-09-18 20:22:33 +0900

    Lesson-9

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
+//URL Constants
+let BASE_URL = "https://chattychatchatting.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//Users Deafults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
